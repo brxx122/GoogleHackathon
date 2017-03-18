1f3a275d53ba56aa0edd3c90d2e5b487eb40ad09
TianxiaoHu <hutianxiao_fdu@126.com>
2017-03-19 01:53:27 +0800

    Return Json

--- back-end/app/views.py
+++ back-end/app/views.py
@@ -1,15 +1,24 @@
-from app import app
-from flask import Flask, render_template, session, request, redirect, flash, g,url_for,jsonify
+from app import app, conn
+from flask import Flask, render_template, url_for,jsonify
 
 @app.route('/')
 def Login():
     return render_template('index.html')
 
-
-@app.route('/_hhh', methods=["POST", "GET"])
-def hhh():
-    dict = {'a': 1, 'b': 2}
-    return jsonify(dict)
+@app.route('/_return_news_by_year')
+def return_news_by_year(year, index):
+    cur = conn.cursor()
+    cur.execute('select * from stories limit 10;')
+    data = cur.fetchall()
+    result = []
+    for i in range(10):
+        subresult = {}
+        subresult['author'] = data[i][1]
+        subresult['title'] = data[i][4]
+        subresult['time'] = data[i][3][:10]
+        subresult['url'] = data[i][5]
+        result.append(subresult)
+    return jsonify(result)
 
 
 @app.route('/_he', methods=["POST", "GET"])
